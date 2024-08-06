73e10b3f867115b7391f3ae56e79d66bdf071756
David Essien <davidessienshare@gmail.com>
2024-08-06 22:15:33 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,41 +5,26 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Business;
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
+Route::view('/services', 'services');
+Route::view('/signup', 'signup');
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/businesses', [BusinessController::class, 'index']);
-Route::get('/businesses/create', [BusinessController::class, 'create']);
-Route::get('/businesses/{business}', [BusinessController::class, 'show']);
-Route::get('/businesses/{business}/edit', [BusinessController::class, 'edit']);
-Route::patch('/businesses/{business}', [BusinessController::class, 'update']);
-Route::delete('/businesses/{business}', [BusinessController::class, 'destroy']);
-Route::post('/business', [BusinessController::class, 'store']);
-
-Route::get('/services', function () {
-    return view('services');
-});
-
-Route::get('/signup', function () {
-    return view('signup');
+Route::controller(BusinessController::class)->group(function () {
+    Route::get('/businesses', 'index');
+    Route::get('/businesses/create', 'create');
+    Route::get('/businesses/{business}', 'show');
+    Route::get('/businesses/{business}/edit', 'edit');
+    Route::patch('/businesses/{business}', 'update');
+    Route::delete('/businesses/{business}', 'destroy');
+    Route::post('/business', 'store');
 });
 
 
 // Users
-Route::get('/users/dashboard', function(){
-    return view('users.dashboard.index');
-});
-
-Route::get('/users/dashboard/businesses', function(){
+Route::view('/users/dashboard', 'users.dashboard.index');
+Route::get('/users/dashboard/businesses', function () {
     $businesses = Business::latest()->paginate(20);
     return view('users.dashboard.show-businesses', ['businesses' => $businesses]);
 });
